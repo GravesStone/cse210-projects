7d12c86bf1225b6d8e8efe0516c27fc9fc4f7366
GravesStone <104344076+GravesStone@users.noreply.github.com>
2023-01-02 13:18:23 +0000

    Changes  - Adding Comments in the programs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,13 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+    // User Input
        Console.WriteLine("Welcome to BYU Calculator Grade");
        Console.WriteLine("What is the grade percentage? ");
        string answer = Console.ReadLine();
+    // Typing in the Screen
        int percentage = int.Parse(answer);
-
+    // Declaire A Integer + Connect in the Answer that the user inputed in Answer
         string letter = "";
-
+    //If else Method 
         if (percentage >= 90)
         {
             letter = "A";
@@ -27,19 +29,22 @@ class Program
         {
             letter = "D";
         }
+        //If the user doesn't meet the requirments on the grade it will automatically Shows F
         else
         {
             letter = "F";
         }
 
         Console.WriteLine($" Your  Grade is {letter}");
+        //End Of the Code Result if Greater than 90-60
         if (percentage >= 70)
         {
             Console.WriteLine("You passed!");
         }
+        // Else if the user has a lower than User < 69 the Program show Better Luck Next time 
         else
         {
-            Console.WriteLine($"Better luck next time Need to improve!!{letter}");
+            Console.WriteLine($"Better luck next time Need to improve!! , {letter}");
         }
     }
 }
