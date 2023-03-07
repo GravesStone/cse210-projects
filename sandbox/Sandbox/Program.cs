ac09a04d35f6059f04763bcdf3b1cf919a530a5c
GravesStone <est22003@byui.edu>
2023-03-07 22:55:03 +0800

    changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,33 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Win a Prize!");
-        Console.Write("Choose a door:  1, 2, or 3: ");
-        string userChoice = Console.ReadLine();
         
-        if (userChoice == "Y")
-        {
-            string message = "You won a new computer!";
-            Console.WriteLine(message);
-        }
-        else if (userChoice == "N" == false)
-        {
-            string message = "You won a new monitor!";
-            Console.WriteLine(message);
-        }
-        else if (userChoice == "3")
-        {
-            string message = "You won a new keyboard!";
-            Console.WriteLine(message);
-        }
-        else
-        {
-            string message = "You lose.  Better luck next time.";
-            Console.WriteLine(message);
-        }
-        
-        Console.ReadLine();
-
     }
        
 }
