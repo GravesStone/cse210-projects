e6cdb54563c404b70c294287b0dcde89291c2503
GravesStone <est22003@byui.edu>
2023-03-23 23:51:18 +0800

    new ccodes

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {   
-        Console.WriteLine("-------------------AmaShuan--------------------------");
+        Console.WriteLine("-------------------Shipping Deatils--------------------------");
         //order 1 
-        Address address1 = new Address ("98 Pleasant Ave.", "Bristol", "CT", "USA");
-        Customer customer1 = new Customer("Nice Guy", address1);
+        Address address1 = new Address ("Pasig ", "Quezon", "City", "Philippines");
+        Customer customer1 = new Customer("Mr.Jay", address1);
 
         double order1ShippingCost = customer1.GetShippingCost();
 
-        Product order1Product1 = new Product("Fuji Apple", "4131", 0.25, 2);
-        Product order1Product2 = new Product("Haas Avocado", "4225", 0.50, 4);
+        Product order1Product1 = new Product("Apples", "4131", 0.25, 2);
+        Product order1Product2 = new Product("Pineapple", "4225", 0.50, 4);
 
         Order order1 = new Order(customer1);
         order1.AddProduct(order1Product1);
@@ -28,18 +28,19 @@ class Program
         order1.DisplayCosts(order1Subtotal ,order1ShippingCost, order1Total);
 
 
-        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine("-------------------Shipping Details--------------------------");
+
 
 
         //order 2
-        Address address2 = new Address ("Rua Garcia de Orta", "Lisbon", "Lisbon", "Portugal");
+        Address address2 = new Address ("Antonio Luna", "Lisbon", "Lisbon", "Portugal");
         Customer customer2 = new Customer("Joao Garcia", address2);
 
         double order2ShippingCost = customer2.GetShippingCost();
 
-        Product order2Product1 = new Product("Husked Coconut", "4260", 2.00, 5);
-        Product order2Product2 = new Product("Red Mango", "4959", 1.00, 4);
-        Product order2Product3 = new Product("Pineapple", "4029", 2.00, 2);
+        Product order2Product1 = new Product("Coconut", "4260", 2.00, 5);
+        Product order2Product2 = new Product("Mango", "4959", 1.00, 4);
+        Product order2Product3 = new Product("Guava", "4029", 2.00, 2);
 
         Order order2 = new Order(customer2);
         order2.AddProduct(order2Product1);
